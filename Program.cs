e003c4b314c6301db7737cbc1a3fa16816dcc034
yakha714 <ntshobaneluyakha9@gmail.com>
2026-04-13 14:33:41 +0200

    Added main entry point and core loop logic .

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Media;
+using CybersecurityAwarenessBot;
+
+namespace CybersecurityAwarenessBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TASK 1: Voice Greeting
+            try
+            {
+                // Ensure the filename here matches your file in Solution Explorer
+                SoundPlayer player = new SoundPlayer("greeting.wav...mpeg");
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Audio Error: " + ex.Message);
+            }
+
+            // TASK 2: UI Header & Logo
+            UIHelper.ShowHeader();
+
+            // TASK 3: Bot Conversation
+            Chatbot myBot = new Chatbot();
+            myBot.Start();
+
+            Console.WriteLine("\n--- Session Active. Press Enter to exit. ---");
+            Console.ReadLine();
+        }
+    }
+}
